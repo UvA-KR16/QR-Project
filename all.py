58f256a35e1850cef1e54a02ac839b579e6f141e
Robert White <ai.robert.wangshuai@gmail.com>
2016-10-15 15:06:40 +0200

    push

--- all.py
+++ all.py
@@ -1,3 +1,4 @@
+import sys
 
 IFZero = 0
 IFPos = 1
@@ -28,12 +29,15 @@ index = 0
 
 class State ():
 	global index
-	def __init__(self, IF, V, OF):
+	def __init__(self, IF, DIF, V, DV, OF, DOF):
 		index = index + 1 
 		self.__index = index + 1
 		self.__IF = IF
+		self.__DIF = DIF
 		self.__V = V
+		self.__DV = DV
 		self.__OF = OF
+		self.__DOF = DOF
 
 
 
@@ -42,42 +46,55 @@ def constraints (state):
 	return []
 
 
+def possibleStates(IF, DIF, V):
+	return []
 
-def main ([IF, DIF, V]):
+def main (IF, DIF, V):
+	global Expanded, ToExpand, Connection
+	
 	print 'I start a first node now'
-	s = State(IF, DIF, V)
-	combinations = constraints(s)
-	global Expanded ToExpand Connection
-	Expanded = Expanded.append(s)
-	for cb in combinations:
-		s = State (cb[0], cb[1], cb [2])
-	if s in Expanded:
-		Connection.append(current.index, s.index)
-	if 
+	possStates = possibleStates(IF, DIF, V)
+	ToExpand.append(possStates)
+	while not len(ToExpand) == 0 :
+		currentState = ToExpand[0]
+		ToExpand.remove (state)
+		Expanded.append(state)
+
+		combinations = constraints(s)
+		for c in combinations:
+			#create a state for each combination
+			s = State(c[0], c[1],c[2],c[3],c[4], c[5], c[6])
+			#build up link between currentState and s
+			Connection.append(currentState.index, s.index)
+			if not(s in ToExpand or s in Expanded):
+				ToExpand.append(s)
+
+	# next, illustrate the state-graph
+	
 
 
 if __name__ == "__main__":
 
-	if (sys.argv[1] == '-all')
+	if (len(sys.argv) >2) and sys.argv[1] == '-all':
 		print 'this is a graph of all states and their transitions'
 	else:
 		print '**********************************************************************************'
 		print 'please input in the following format: inflow(IF)|derivative of inflow (DIF)|volume'
 		print '+ represents positive, - represents negative, 0 represents zero and M represents Max'
-		print 'for example: +0+|--+|+, which means that there is inflow and then it stopped for ',
+		print 'for example: +|-+|+, which means that there is inflow and then it stopped for '
 		print '             a while and it continued for a sink with some water in it initially' 
 		print '**********************************************************************************'
 		description = raw_input('>')
 		description = description.split('|')
 		print description
-		IF = []
+		IF = 0
 		DIF = []
-		V = []
-		for a in description[0]:
-			if a =='+':
-				IF.append(IFPos)
-			if a == '0':
-				DIF.append(IFZero)
+		V = 0
+		if description[0][0] == '+':
+			IF = IFPos
+		if description[0][0] == '0':
+			IF = IFZero
+
 		for b in description[1]:
 			if b == '-':
 				DIF.append(DIFNeg)
@@ -86,12 +103,10 @@ if __name__ == "__main__":
 			if b =='0':
 				DIF.append(DIFZero)
 		if description[2][0] == '0':
-			V.append(VZero)
+			V = VZero
 		if description[2][0] == 'M':
-			V.append(VMax)
+			V = VMax
 		if description[2][0] == '+':
-			V.append(VMax)
+			V = VMax
 
-	# print IF, ' - ' , DIF, ' - ', V
-
-	main([IF, DIF, V])
+		main(IF, DIF, V)
